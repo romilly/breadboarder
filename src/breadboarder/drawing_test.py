1c1bdec1c2ae08feda107edaa85cebe7dd82b98b
Romilly Cocking <romilly.cocking@gmail.com>
2017-08-22 10:50:12 +0100

    Separated production code from tests.

--- src/breadboarder/drawing_test.py
+++ src/breadboarder/drawing_test.py
@@ -1,24 +1,7 @@
 from unittest import TestCase
-from xml.etree.ElementTree import Element, tostring
 from bs4 import BeautifulSoup
 
-class Drawing(object):
-    def __init__(self):
-        self._children = []
-
-    def svg(self):
-        svg = Element('svg', height='480', width='640')
-        for child in self._children:
-            svg.append(child.svg())
-        return tostring(svg)
-
-    def add(self, item):
-        self._children.append(item)
-
-
-class Breadboard(object):
-    def svg(self):
-        return Element('g',id='breadboard')
+from breadboarder.drawing import Drawing, Breadboard
 
 
 class DrawingTest(TestCase):
