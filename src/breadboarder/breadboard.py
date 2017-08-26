6eb93df7e914cc6aa162c47fe6962e12c6370c5d
Romilly Cocking <romilly.cocking@gmail.com>
2017-08-26 11:20:34 +0100

    Removed TODO

--- src/breadboarder/breadboard.py
+++ src/breadboarder/breadboard.py
@@ -96,7 +96,6 @@ class Breadboard(CompositeItem):
         for i in range(count):
             self.add(Text(str(i+1), Point(self.inset - 1 + self.PITCH * i, vertical_location), anchor=anchor, size=6).rotate(-90))
 
-    # TODO: refactor to take Point(inset,drop) as parameter
     def add_alpha_labels(self, offset_to_letters, letters):
         for i in range(len(letters)):
             self.add(Text(letters[i], offset_to_letters +Point(0,self.PITCH*i), size=6 ).rotate(-90))
