5aacc21bc1bc5e7397f1bc92d69e6f65b9b95171
Romilly Cocking <romilly.cocking@gmail.com>
2017-09-05 10:57:04 +0100

    Added plan mind-map

---
+++ plan/Boarder-plan.mm
@@ -0,0 +1,194 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Boarder plan" FOLDED="false" ID="Freemind_Link_1331878192" CREATED="1266488194781" MODIFIED="1503322365392"><hook NAME="MapStyle">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Goal" POSITION="right" ID="Freemind_Link_1242386705" CREATED="1266488217621" MODIFIED="1485097924419" COLOR="#37a3cb">
+<node TEXT="create breadboard drawing DSL" ID="ID_621181827" CREATED="1503322368524" MODIFIED="1503322381712"/>
+<node TEXT="document (video) development process" ID="ID_1731833462" CREATED="1503322382124" MODIFIED="1503322401303"/>
+</node>
+<node TEXT="Plan" POSITION="right" ID="_" CREATED="1266488212530" MODIFIED="1266488546784" COLOR="#373dcb">
+<edge COLOR="#808080"/>
+<node TEXT="define Iterations" ID="ID_915614811" CREATED="1503322406652" MODIFIED="1503322416128">
+<node TEXT="Iteration 1" ID="ID_463854862" CREATED="1503322474276" MODIFIED="1503322478935">
+<node TEXT="draw breadboard" ID="ID_1481033296" CREATED="1503322480620" MODIFIED="1503322489142">
+<node TEXT="set up project" ID="ID_1378110888" CREATED="1503322785228" MODIFIED="1503322794334">
+<node TEXT="breadboarder" ID="ID_1276319019" CREATED="1503327418102" MODIFIED="1503327422565"/>
+<node TEXT="Python 2.7" ID="ID_336140110" CREATED="1503327423014" MODIFIED="1503753348679"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Python 3 caused problems with bytes vs strings.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="create boarder module" ID="ID_968697022" CREATED="1503322800948" MODIFIED="1503322828281"/>
+<node TEXT="write E2E test" ID="ID_1229691616" CREATED="1503322828828" MODIFIED="1503753360726">
+<node TEXT="fail" ID="ID_1706996076" CREATED="1503322859724" MODIFIED="1503753370957">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="rectangle" ID="ID_1009055803" CREATED="1503322907492" MODIFIED="1503753374500">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="power lines" ID="ID_1746821399" CREATED="1503322916644" MODIFIED="1503753384268">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="dots" ID="ID_519010524" CREATED="1503322922092" MODIFIED="1503753388108">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="letters" ID="ID_1872395683" CREATED="1503322930036" MODIFIED="1503753392524">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="numbers" ID="ID_845405690" CREATED="1503322933644" MODIFIED="1503753396916">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Iteration 2" ID="ID_1425797160" CREATED="1503322496068" MODIFIED="1503322500541">
+<node TEXT="draw wires" ID="ID_920963013" CREATED="1503322528820" MODIFIED="1503753429580">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="image based test?" ID="ID_319093274" CREATED="1503322948204" MODIFIED="1503322954105">
+<node TEXT="take photo of real breadboard" ID="ID_220619505" CREATED="1503322964988" MODIFIED="1503753426140">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node TEXT="Iteration 3" ID="ID_1906408043" CREATED="1503322522132" MODIFIED="1503322526205">
+<node TEXT="add atmega chip" ID="ID_1720290822" CREATED="1503322508700" MODIFIED="1503866162146">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node TEXT="Iteration 4" ID="ID_1109480850" CREATED="1503322539756" MODIFIED="1503322544391">
+<node TEXT="draw components" ID="ID_41161072" CREATED="1503322545300" MODIFIED="1503322557103">
+<node TEXT="resistor" ID="ID_1269198528" CREATED="1503322558148" MODIFIED="1504605099003">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="capacitor" ID="ID_328735972" CREATED="1503322562524" MODIFIED="1503322609932"/>
+<node TEXT="xtal" ID="ID_982504514" CREATED="1503322566892" MODIFIED="1504605102930">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="header" ID="ID_814598911" CREATED="1503322578900" MODIFIED="1503322593621">
+<node TEXT="male" ID="ID_1890153202" CREATED="1504605115170" MODIFIED="1504605117404"/>
+<node TEXT="female" ID="ID_859722054" CREATED="1504605117834" MODIFIED="1504605121366"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="code/video" ID="ID_154137008" CREATED="1503322653988" MODIFIED="1504605150876">
+<icon BUILTIN="button_ok"/>
+<node TEXT="script" ID="ID_270321197" CREATED="1503322670684" MODIFIED="1503322681477"/>
+<node TEXT="take video" ID="ID_1627895091" CREATED="1503322684021" MODIFIED="1503322689896"/>
+<node TEXT="commit" ID="ID_1911092109" CREATED="1503322692628" MODIFIED="1503322704728"/>
+</node>
+<node TEXT="presentation" ID="ID_760953554" CREATED="1503322715724" MODIFIED="1504605153482">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="review" ID="ID_1069265110" CREATED="1503322722820" MODIFIED="1503322724869">
+<node TEXT="projector resolution was terrible" ID="ID_477775845" CREATED="1504605156362" MODIFIED="1504605181618">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="winged it" ID="ID_800180281" CREATED="1504605186834" MODIFIED="1504605216202">
+<icon BUILTIN="button_ok"/>
+<icon BUILTIN="ksmiletris"/>
+</node>
+</node>
+</node>
+<node TEXT="Tasks" POSITION="right" ID="Freemind_Link_857865306" CREATED="1266488223477" MODIFIED="1266488636684" COLOR="#de5613"/>
+<node TEXT="Review" FOLDED="true" POSITION="left" ID="Freemind_Link_1153466861" CREATED="1266488259116" MODIFIED="1266488895851" COLOR="#fb1802">
+<icon BUILTIN="xmag"/>
+<node TEXT="WW" ID="Freemind_Link_1466796877" CREATED="1266488965666" MODIFIED="1296303686570" COLOR="#ffcc00" BACKGROUND_COLOR="#006666">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="WDNW" ID="Freemind_Link_1910608691" CREATED="1266488980018" MODIFIED="1296303689746">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node TEXT="WDD" ID="Freemind_Link_1294877740" CREATED="1266488984697" MODIFIED="1296303577864" COLOR="#00cc00">
+<icon BUILTIN="licq"/>
+</node>
+<node TEXT="Questions" ID="Freemind_Link_1003502265" CREATED="1266489024048" MODIFIED="1296303570801" COLOR="#00cccc">
+<icon BUILTIN="help"/>
+</node>
+<node TEXT="Surprises" ID="Freemind_Link_19217072" CREATED="1266489032944" MODIFIED="1296303119998" COLOR="#ff0000" BACKGROUND_COLOR="#ffff00">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node TEXT="Feelings" ID="Freemind_Link_1706517660" CREATED="1266489043119" MODIFIED="1296303692921" COLOR="#cc0000">
+<icon BUILTIN="ksmiletris"/>
+</node>
+<node TEXT="Ideas" ID="Freemind_Link_1517433488" CREATED="1266488273147" MODIFIED="1266488921179" COLOR="#36d12a">
+<icon BUILTIN="idea"/>
+</node>
+<node TEXT="Fridge" ID="Freemind_Link_1954142386" CREATED="1266488793745" MODIFIED="1266488959040" COLOR="#9679df">
+<icon BUILTIN="desktop_new"/>
+</node>
+<node TEXT="discoveries" ID="ID_1011483963" CREATED="1485097938875" MODIFIED="1485097984855" COLOR="#00cc33">
+<icon BUILTIN="very_positive"/>
+</node>
+</node>
+<node TEXT="Record" POSITION="left" ID="Freemind_Link_813012694" CREATED="1266488248876" MODIFIED="1266488728945" COLOR="#27444b">
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="Resources" POSITION="left" ID="Freemind_Link_1594441695" CREATED="1273319832507" MODIFIED="1296303609303" COLOR="#33ff33">
+<icon BUILTIN="password"/>
+</node>
+</node>
+</map>
