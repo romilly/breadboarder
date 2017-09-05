0b6e0494ed616a1308dfc0c65bd3244c7544bb1b
Romilly Cocking <romilly.cocking@gmail.com>
2017-09-05 08:55:41 +0100

    Refactored out TwoPinUnpolarised

--- src/breadboarder/components.py
+++ src/breadboarder/components.py
@@ -66,13 +66,10 @@ class Wire(Line, Component):
         return self
 
 
-class Crystal(GroupedDrawable, Component):
-    def __init__(self, frequency):
-        GroupedDrawable.__init__(self, svg_id='Crystal')
-        self.frequency = frequency
+class TwoPinUnpolarised(GroupedDrawable, Component):
+    def __init__(self, svg_id):
+        GroupedDrawable.__init__(self, svg_id=svg_id)
         self.end = Point(0,0)
-        self.body_width = 2 * Breadboard.PITCH
-        self.body_height = Breadboard.PITCH
 
     def connect(self, positions):
         start, end = positions
@@ -84,42 +81,16 @@ class Crystal(GroupedDrawable, Component):
     def set_end(self, end):
         self.end = end
 
-    def add_elements(self):
-        # coordinates are relative to the Crystal's's start
-        extent = self.end - self.start
-        length = extent.r()
-        theta = extent.theta()
-        total_wire_length = length - self.body_width
-        offset = Point(total_wire_length, -self.body_height).scale(0.5)
-        self.add(horizontal_line(Point(0,0), length, color='grey', stroke_width=2, linecap='round'))
-        body = GroupedDrawable(svg_id='resistor body')
-        rectangle = Rectangle(self.body_width, self.body_height, fill='gray', stroke='lightgray', rx='4', ry='4')
-        body.add(rectangle)
-        body.add(Text(self.frequency, rectangle.center()+Point(0,1.5), anchor='middle', color='black', size=3))
-        self.rotate(theta, self.start)
-        self.add(body.move_to(offset))
 
-
-class Resistor(GroupedDrawable, Component):
+class Resistor(TwoPinUnpolarised):
     def __init__(self, resistance):
-        GroupedDrawable.__init__(self, svg_id='Resistor')
+        TwoPinUnpolarised.__init__(self, svg_id='Resistor')
         self.band_height = Breadboard.PITCH-1
         self.band_width = 2
         self.body_width = 3 * Breadboard.PITCH
         self.body_height = Breadboard.PITCH
         self.resistance = resistance
-        self.end = Point(0,0)
         self.coder = ColorCode()
-
-    def connect(self, positions):
-        start, end = positions
-        self.move_to(start)
-        self.set_end(end)
-        self.add_elements()
-        return self
-
-    def set_end(self, end):
-        self.end = end
 
     def add_elements(self):
         # coordinates are relative to the Resistor's start
@@ -141,3 +112,27 @@ class Resistor(GroupedDrawable, Component):
         band_colors = self.coder.bands_for(self.coder.parse(self.resistance))
         for (i, band) in enumerate(band_colors):
             body.add(Rectangle(self.band_width, self.band_height, fill=band, stroke=None).move_to(Point(5 + 5*i,0.5)))
+
+
+class Crystal(TwoPinUnpolarised):
+    def __init__(self, frequency):
+        TwoPinUnpolarised.__init__(self, svg_id='Crystal')
+        self.frequency = frequency
+        self.body_width = 2 * Breadboard.PITCH
+        self.body_height = Breadboard.PITCH
+
+    def add_elements(self):
+        # coordinates are relative to the Crystal's's start
+        extent = self.end - self.start
+        length = extent.r()
+        theta = extent.theta()
+        total_wire_length = length - self.body_width
+        offset = Point(total_wire_length, -self.body_height).scale(0.5)
+        self.add(horizontal_line(Point(0,0), length, color='grey', stroke_width=2, linecap='round'))
+        body = GroupedDrawable(svg_id='resistor body')
+        rectangle = Rectangle(self.body_width, self.body_height, fill='gray', stroke='lightgray', rx='4', ry='4')
+        body.add(rectangle)
+        body.add(Text(self.frequency, rectangle.center()+Point(0,1.5), anchor='middle', color='black', size=3))
+        self.rotate(theta, self.start)
+        self.add(body.move_to(offset))
+
