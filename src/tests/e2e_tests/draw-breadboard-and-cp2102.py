7d587dc26d474d5e5f614ffec6115a2094ecea4c
Romilly Cocking <romilly.cocking@gmail.com>
2019-07-26 11:12:54 +0100

    unscrambling the mess :) lots works, but tests now don't. Fixing 'em'

--- src/tests/e2e_tests/draw-breadboard-and-cp2102.py
+++ src/tests/e2e_tests/draw-breadboard-and-cp2102.py
@@ -1,7 +1,8 @@
+from breadboarder.author.illustrator import Illustrator
 from breadboarder.core.breadboard import Breadboard
 from breadboarder.core.cp2102 import CP2102
 from breadboarder.core.project import Project
-from breadboarder.svg.svg import Point, write
+from svg.svg import Point, write
 
 
 def draw_breadboard():
@@ -10,8 +11,10 @@ def draw_breadboard():
     project.add(breadboard)
     cp = CP2102().rotate(-90, Point(40, 400)).move_to(Point(40,400))
     project.add(cp)
-    svg = project.element()
-    write(svg, 'svg/two-hosts.svg')
+    illustrator = Illustrator()
+    for step in project.steps():
+        illustrator.take(step)
+    write(illustrator.svg(), 'svg/two-hosts.svg')
 
 
 draw_breadboard()
